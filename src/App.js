be7b2c4eef211807e7daefeaddce73cd8279ec5f
Marko Frntić <mfrntic@gmail.com>
2024-01-15 15:05:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, MenuButton, tokens, makeStyles, mergeClasses } from '@fluentui/react-components';
+
+import {
+  DrawerBody,
+  DrawerHeader,
+  DrawerHeaderTitle,
+  Drawer,
+  DrawerProps,
+
+  Label,
+  Radio,
+  RadioGroup,
+  shorthands,
+  useId,
+} from "@fluentui/react-components";
+import { Dismiss24Regular } from "@fluentui/react-icons";
+import { useState } from 'react';
+
+const useStyle = makeStyles({
+  header: { backgroundColor: tokens.colorBrandBackground, color: tokens.colorBrandBackgroundInverted },
+  footer: { backgroundColor: tokens.colorNeutralBackgroundInverted, color: tokens.colorNeutralBackground1 },
+  sidebar: { backgroundColor: tokens.colorNeutralBackground4 }
+})
 
 function App() {
+  const style = useStyle();
+
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className={mergeClasses('header', style.header)}>
+        <div className="headerTitle"> SSPP</div>
+        <MenuButton appearance='primary'>Izbornik 1</MenuButton>
+        <MenuButton appearance='primary'>Izbornik 2</MenuButton>
+        <MenuButton appearance='primary'>Izbornik 3</MenuButton>
+        <Button appearance='primary'>Izbornik 4</Button>
+      </div>
+      <div className="body">
+        <Drawer
+          type="inline"
+          separator
+          open={isOpen}
+          onOpenChange={(_, { open }) => setIsOpen(open)}
         >
-          Learn React
-        </a>
-      </header>
+          <DrawerHeader>
+            <DrawerHeaderTitle
+              action={
+                <Button
+                  appearance="subtle"
+                  aria-label="Close"
+                  icon={<Dismiss24Regular />}
+                  onClick={() => setIsOpen(false)}
+                />
+              }
+            >
+              Default Drawer
+            </DrawerHeaderTitle>
+          </DrawerHeader>
+
+          <DrawerBody>
+            <p>Drawer content</p>
+          </DrawerBody>
+        </Drawer>
+
+        <div className="content">
+
+          Body content
+          <Button appearance='primary' onClick={() => setIsOpen(a => !a)}>Toggle Menu</Button>
+
+        </div>
+
+      </div>
+      <div className={mergeClasses('footer', style.footer)}>Footer</div>
     </div>
   );
 }
